bf5108cf8f7d551025ed607c9bc3bef8e557631c
suri <54814232+xSuri@users.noreply.github.com>
2020-08-17 20:56:50 +0200

    Add files via upload

--- main/bot.js
+++ main/bot.js
@@ -39,6 +39,11 @@ client.on("ready", () => { // If bot run showing bot info...
 
 client.on('message', msg => {
 
+    if ( msg.content.startsWith('.helpBot') ){ // Function write message to DB (table messages)
+        let text = 'prefix=`"."` \n Template: `prefixCommand` \n\n Commands: \n\n `.helpBot` \n  `.writeMSG` [ message ] - Writting message to DB. \n `.getMSG` [ id ] - Getting message. \n `.getIdMSG` [ id ] - Getiing message, message GID ( Guild ID ) and MID ( Message ID )'
+        msg.channel.send(text)
+    }
+
     if ( msg.content.startsWith('.writeMSG') ){ // Function write message to DB (table messages)
 
         var sql = "INSERT INTO messages (id , nick, message, guild, date) VALUES (NULL,"+"'"+msg.author.username+"'"+","+"'"+msg.content.slice(9)+"'"+","+"'"+msg.guild.name+"'"+", NOW())";
@@ -46,13 +51,32 @@ client.on('message', msg => {
           if (err) throw err;
           // If insert info ^ this is showing in chat.
           console.log("1 message write");
-          msg.channel.send("Written message.");
+          msg.channel.send("`Written message.`");
         });
+
+        con.query("SELECT * FROM messages", function (err, result, fields) { // Taking all from messages
+            if (err) throw err;
+
+            var howmuch = 0; // How much user message 
+            
+            for (var x=0; x != result.length; x++){
+                if ( result[x]["nick"] == msg.author.username &&  result[x]["guild"] == msg.guild.name){
+
+                    howmuch += 1;
+
+                    if (x==result.length-1){
+
+                        msg.channel.send("`You have "+howmuch+" saved message`")
+
+                    }
+                }
+            }
+        })
     
     }
-    if ( msg.content.startsWith(".getMG")){ // Function get message from DB (table messages)
+    if ( msg.content.startsWith(".getMSG")){ // Function get message from DB (table messages)
 
-        const args = msg.content.slice(6).trim().split(/ +/); // argument and slice 6 <-- (".getMG")
+        const args = msg.content.slice(7).trim().split(/ +/); // argument and slice 6 <-- (".getMG")
 
         con.query("SELECT * FROM messages", function (err, result, fields) { // Taking all from messages
             if (err) throw err;
@@ -63,23 +87,15 @@ client.on('message', msg => {
                 if ( result[x]["nick"] == msg.author.username &&  result[x]["guild"] == msg.guild.name){
     
                     howmuch += 1;
-                    console.log("Messages: "+howmuch)
-                    // if ( x==result.length-1 ){ // Value user saved message
-                    //     msg.channel.send("You have "+howmuch+" saved message.")
-                    // }
+
                     if ( howmuch == args[0]){ // Showing user message (id)
-                        msg.channel.send(result[x]["nick"]+" // "+result[x]["message"]);
+                        msg.channel.send("`"+result[x]["nick"]+" // "+result[x]["message"]+"`");
                     }
                     else if ( howmuch < args[0]-1 ){ // If user dont saved messages
-                        msg.channel.send("DB dont have msg.")
+                        msg.channel.send("`DB dont have msg.`")
                         x=result.length-1;
                     }
                 
-                }
-                else if ( x==result.length-1 && result[x]["nick"] != msg.author.username ){ // Secure to saved message to one guild
-    
-                    msg.channel.send("This message is no saved in this guild or don't exit.(?)");
-    
                 }
             }
     
@@ -87,6 +103,36 @@ client.on('message', msg => {
     
 
     }   
+
+    if ( msg.content.startsWith(".getIdMSG")){ // Function get message from DB (table messages)
+
+        const args = msg.content.slice(9).trim().split(/ +/); // argument and slice 6 <-- (".getMG")
+
+        con.query("SELECT * FROM messages", function (err, result, fields) { // Taking all from messages
+            if (err) throw err;
+
+            var howmuch = 0; // How much user message 
+            
+            for (var x=0; x != result.length; x++){
+                if ( result[x]["nick"] == msg.author.username &&  result[x]["guild"] == msg.guild.name){
+    
+                    howmuch += 1;
+
+                    if ( howmuch == args[0]){ // Showing user message (id)
+                        msg.channel.send("`(GID: "+howmuch+") "+result[x]["nick"]+" // "+result[x]["message"] + " (MID: "+result[x]['id']+")`");
+                    }
+                    else if ( howmuch < args[0]-1 ){ // If user dont saved messages
+                        msg.channel.send("`DB dont have msg.`")
+                        x=result.length-1;
+                    }
+                
+                }
+            }
+    
+        })
+    
+
+    } 
 })
 //auth
 
